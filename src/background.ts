ec1538a7f6d476be7248ae96a4eb3b124d912b78
Ebihara Yusuke <yusuke@ebihara.me>
2023-07-30 02:06:57 +0900

    fix: 日付を挿入する時にエラーになるバグを修正

--- src/background.ts
+++ src/background.ts
@@ -33,52 +33,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             page_size: 50,
         });
 
-        const target = blockList.results.find((block: any) => {
+        const dateBlock = blockList.results.find((block: any) => {
             return (
                 block.type === "heading_2" &&
                 block.heading_2.rich_text[0].text.content === dateString
             );
         });
-
-        if (!target) {
-            console.log("Creating today's block");
-            await notion.blocks.children.append({
-                block_id: note,
-                after: blockList.results[0].id,
-                children: [
-                    {
-                        object: "block",
-                        type: "heading_2",
-                        heading_2: {
-                            rich_text: [
-                                {
-                                    type: "text",
-                                    text: {
-                                        content: dateString,
-                                    },
-                                },
-                            ],
-                        },
-                    },
-                ],
-            });
-            console.log("Created today's block");
-        }
-
-        await sleep(200);
-
-        const createdTarget = blockList.results.find((block) => {
-            if (!("type" in block)) return false;
-            return (
-                block.type === "heading_2" &&
-                // @ts-ignore
-                block.heading_2.rich_text[0].text.content === dateString
-            );
-        });
-        if (!createdTarget) {
-            console.error("Cannot find today's block");
-            return;
-        }
 
         const blocks = filteredTabs.map((tab) => {
             return {
@@ -100,12 +60,38 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             };
         });
 
-        await notion.blocks.children.append({
-            block_id: note,
-            after: createdTarget.id,
-            // @ts-ignore
-            children: blocks,
-        });
+        const newDateBlock = {
+            object: "block",
+            type: "heading_2",
+            heading_2: {
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: dateString,
+                        },
+                    },
+                ],
+            },
+        };
+
+        if (dateBlock) {
+            await notion.blocks.children.append({
+                block_id: note,
+                after: dateBlock.id,
+                // @ts-ignore
+                children: blocks,
+            });
+        } else {
+            console.log("Creating today's block");
+            await notion.blocks.children.append({
+                block_id: note,
+                after: blockList.results[0].id,
+                // @ts-ignore
+                children: [newDateBlock, ...blocks],
+            });
+            console.log("Created today's block");
+        }
 
         console.log("Complete: added blocks to Notion");
 
